adf994448728b122912eafeb29d3046f48d337f3
Voxuel <Leo.fridh@hotmail.com>
2023-02-24 13:13:52 +0100

    add js

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+
+function getNav(params) {
+    $("#nav-bar").load("nav.html");
+};
+
+function navigation(){
+    load("nav.html")
+};
+
+navigation(document.getElementById("buildNavigation"))
+
+
+
