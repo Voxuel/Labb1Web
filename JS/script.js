a13e637857dbc467b3eaaf3962c44b1c916f0b86
Voxuel <Leo.Fridh@hotmail.com>
2023-03-08 15:42:57 +0100

    add json read on workplaces

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,24 @@
 
+
+function readCvFromJson(){
+    fetch('../assets/cv.json')
+    .then((response) => response.json())
+    .then((data) => 
+    {
+        getWorkExperience(data["Work"]);
+})}
+
+function getWorkExperience(work){
+    var listDiv = document.getElementById('exp_list');
+    var ul = document.createElement('ul');
+
+    console.log(work);
+
+    for(let i = 0; i < work.length; i++){
+        var li = document.createElement('li');
+        li.innerHTML = work[i].Place + '<br>' + work[i].YearsOfEmployment + '<br>' + work[i].Description;
+        ul.appendChild(li)
+    }
+    listDiv.appendChild(ul);
+
+}
